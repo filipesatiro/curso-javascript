14c005d306e0cbd30e1be6405e5427fd5bbf501f
filipe satiro <filipelipe202229@gmail.com>
2025-06-14 04:58:39 -0300

    criado o arquivo sintaxe: que explica que JS é CaseSensityve e tem diferença entre números com aspas dupla e numeros sem estar entre aspas

---
+++ paginas-javascript/sintaxe.js
@@ -0,0 +1,5 @@
+var PESSOA = "dimitri";
+var pessoa = "filipe";
+console.log(pessoa);
+/* sintaxe é a as pequenas diferenças exemplo o numero 1 sem ("" aspas) quer dizer que é numero e se tiver com (""aspas) ele é considerado uma palavra */
+// o javascript é case sensitive//
