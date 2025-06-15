16d6d6db2f1845717b2a07ab989b546eef5352e6
filipe satiro <filipelipe202229@gmail.com>
2025-06-15 06:20:47 -0300

    criado a pasta objetos onde é colocados varios valores em uma variavel

---
+++ paginas-javascript/objetos.js
@@ -0,0 +1,33 @@
+/* OBJETOS
+Objetos são basicamente variáveis com muitos valores dentro.
+
+Ex: Const carro = {marca = "ford", modelo: "ka", ano = "2015"}
+
+Os valores dentro de um objeto são chamados propriedades.
+
+Objetos também podem ter métodos. Um Método é uma função colocadas dentro de uma propriedade.
+*/
+const carro = {
+    marca:"ford",
+    modelo:"ka",
+    ano:"2015",
+    placa:"ABC-1234",
+    buzina: function(){alert('biiiiiiii')},
+    completo: function(){
+        return "A marca é " + this.marca + " e o modelo é: "+ this.modelo
+    }
+};
+console.log(carro.completo());
+
+
+
+
+
+
+
+
+
+
+
+
+
